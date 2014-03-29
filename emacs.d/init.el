d015087f08aa888c8e9b822ce309c343dd0c84d9
Corin Schedler <corin@dubdromic.com>
2014-03-29 16:24:12 -0500

    Add org-agenda directory, add ~/Bin to emacs path

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -19,10 +19,15 @@
 (load "syntax-table.el")
 
 ;; robe-mode needs rbenv goodness
-(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "HOME") "/Bin:" (getenv "PATH")))
 (setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
+ '(org-agenda-files (quote ("~/Org/RT")))
  '(rspec-key-command-prefix (kbd "M-s"))
  '(rspec-use-zeus-when-possible t))
