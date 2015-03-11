25e773735c6d29141a66bb0141795976a7c79019
Corin Schedler <corin@dubdromic.com>
2015-03-11 11:19:10 -0500

    Clean up init file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -30,37 +30,26 @@
 (load "smooth-scrolling-defaults.el")
 
 ;; robe-mode needs rbenv goodness
-(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "HOME") "/Bin:" (getenv "PATH")))
-(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
+(setenv "PATH"
+        (concat
+         (getenv "HOME") "/.rbenv/shims:"
+         (getenv "HOME") "/.rbenv/bin:"
+         (getenv "HOME") "/Bin:"
+         (getenv "PATH")))
+(setq exec-path
+      (cons (concat
+             (getenv "HOME") "/.rbenv/shims")
+            (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(cider-lein-command "/home/corin/Bin/lein")
  '(coffee-tab-width 2)
  '(css-indent-offset 2)
- '(erc-hide-list (quote ("JOIN" "PART" "QUIT")))
- '(erc-modules
-   (quote
-    (button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands notifications readonly ring services stamp track)))
- '(erc-nick "dubdromic")
- '(inf-ruby-default-implementation "pry")
- '(inf-ruby-implementations
-   (quote
-    (("ruby" . "bundle exec irb --prompt default --noreadline -r irb/completion")
-     ("jruby" . "jruby -S irb --prompt default --noreadline -r irb/completion")
-     ("rubinius" . "rbx -r irb/completion")
-     ("yarv" . "irb1.9 -r irb/completion")
-     ("macruby" . "macirb -r irb/completion")
-     ("pry" . "bundle exec pry"))))
  '(magit-item-highlight-face (quote bold))
  '(magit-use-overlays nil)
  '(nyan-animate-nyancat nil)
  '(nyan-mode t)
  '(nyan-wavy-trail t)
- '(org-agenda-files (quote ("~/Org/RT")))
  '(rspec-key-command-prefix (kbd "M-s"))
  '(rspec-use-rake-when-possible nil)
  '(rspec-use-zeus-when-possible t)
@@ -68,9 +57,4 @@
  '(ruby-deep-arglist nil)
  '(ruby-deep-indent-paren nil)
  '(safe-local-variable-values (quote ((buffer-file-coding-system . utf-8)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
